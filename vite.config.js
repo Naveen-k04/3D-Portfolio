3b74da483c6fe4dafb7401208cf77e961791bc55
Naveen-k <naveensarita008@gmail.com>
2025-04-10 15:55:21 +0530

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -1,9 +1,7 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
 
-// https://vitejs.dev/config/
 export default defineConfig({
-  base: '/naveens.github.io/'
   plugins: [react()],
-  
-})
+  base: '/naveens.github.io/', // <- this must match your repo name
+});
